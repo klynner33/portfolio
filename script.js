3abbf87e02b7478ddc68e0b1922b2fcfb67b3f9b
klynner33 <113389068+klynner33@users.noreply.github.com>
2024-05-30 22:49:59 -0500

    Add hamburger menu

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const tabLinks = document.querySelectorAll('.tab-links');
 const tabContents = document.querySelectorAll('.tab-contents');
 
 tabLinks.forEach((tab, index) => {
-  tab.addEventListener('click', function() {
+  tab.addEventListener('click', () => {
       tabLinks.forEach(link => link.classList.remove('active-link'));
     
       tab.classList.add('active-link');
@@ -12,3 +12,14 @@ tabLinks.forEach((tab, index) => {
       tabContents[index].classList.add('active-tab');
   });
 });
+
+const closeMenuButton = document.querySelector('.fa-xmark');
+const openMenuButton = document.querySelector('.fa-bars');
+const sideMenu = document.querySelector('.side-menu');
+
+openMenuButton.addEventListener('click', () => {
+  sideMenu.style.right = "0";
+})
+closeMenuButton.addEventListener('click', () => {
+  sideMenu.style.right = "-200px";
+})
